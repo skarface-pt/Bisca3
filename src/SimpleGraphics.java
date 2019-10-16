132621cdf293f8245686e2abbef200055115bbf9
Code Cadet <mmgoncalvess@gmail.com>
2019-10-16 10:21:56 +0100

    Work progress

--- src/SimpleGraphics.java
+++ src/SimpleGraphics.java
@@ -1,5 +1,7 @@
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Shape;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 
 public class SimpleGraphics {
@@ -10,9 +12,9 @@ public class SimpleGraphics {
         int canvasHigh = 900;
 
         Rectangle rect1 = new Rectangle(10, 10, canvasWidth, canvasHigh);
-        Rectangle rect2 = new Rectangle(538, 30, 115, 160);
-        Rectangle rect3 = new Rectangle(693, 30, 115, 160);
-        Rectangle rect4 = new Rectangle(838, 30, 115, 160);
+        //Rectangle rect2 = new Rectangle(538, 30, 115, 160);
+        //Rectangle rect3 = new Rectangle(693, 30, 115, 160);
+        //Rectangle rect4 = new Rectangle(838, 30, 115, 160);
         Rectangle rect5 = new Rectangle(1327, 30, 115, 160);
 
         Rectangle rect6 = new Rectangle(60, 370, 115, 160);
@@ -27,11 +29,10 @@ public class SimpleGraphics {
         Rectangle rect13 = new Rectangle(1327, 710, 115, 160);
 
 
-
         rect1.setColor(Color.LIGHT_GRAY);
-        rect2.setColor(Color.BLUE);
-        rect3.setColor(Color.BLUE);
-        rect4.setColor(Color.BLUE);
+        //rect2.setColor(Color.BLUE);
+        //rect3.setColor(Color.BLUE);
+        //rect4.setColor(Color.BLUE);
         rect5.setColor(Color.BLUE);
         rect7.setColor(Color.BLUE);
         rect8.setColor(Color.BLUE);
@@ -42,11 +43,10 @@ public class SimpleGraphics {
         rect13.setColor(Color.BLUE);
 
 
-
         rect1.fill();
-        rect2.fill();
-        rect3.fill();
-        rect4.fill();
+        //rect2.fill();
+        //rect3.fill();
+        //rect4.fill();
         rect5.fill();
         rect6.fill();
         rect7.fill();
@@ -57,17 +57,30 @@ public class SimpleGraphics {
         rect12.fill();
         rect13.fill();
 
+        //Picture pic = new Picture(60, 370, "/Users/codecadet/Documents/briscolaofthree_pt/PNG/little.png");
+        Picture pic = new Picture(688, 30, "PNG/1.png");
+        pic.draw();
+
+        Picture pic2 = new Picture(538, 30, "PNG/2.png");
+        pic2.draw();
+
+        Picture pic3 = new Picture(838, 30, "PNG/3.png");
+        pic3.draw();
+
+        Picture pic4 = new Picture(340, 330, "PNG/4.png");
+        pic4.draw();
+
+        Picture pic5 = new Picture(300, 370, "PNG/RedCover.png");
+        pic5.draw();
 
 
+      
 
 
-
-
-
+        System.out.println(pic.pixels());
 
 
     }
-
 
 
 }
